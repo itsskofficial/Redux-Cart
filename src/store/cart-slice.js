fb586e7c05b91e4a7f85b9f3d5e67a39cd7e1e93
SK <sarthakkarandikar03@gmail.com>
2023-02-22 11:55:51 +0000

    Feb 22, 2023, 5:25 PM

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -24,5 +24,23 @@ const cartSlice = createSlice({
                 existingItem.totalPrice=existingItem.totalPrice+newItem.price
             }
         }
+
+        addItem(state,action) {
+            const newItem = action.payload
+            const existingItem = state.items.find(item => item.id === newItem.id)
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    name: newItem.title,
+                    totalPrice:newItem.price
+                })
+            }
+            else {
+                existingItem.quantity++
+                existingItem.totalPrice=existingItem.totalPrice+newItem.price
+            }
+        }
     }
 })
