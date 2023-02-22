177be10743b089b318498f3d9722e748f1fe041c
SK <sarthakkarandikar03@gmail.com>
2023-02-22 11:51:52 +0000

    Feb 22, 2023, 5:21 PM

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -20,7 +20,8 @@ const cartSlice = createSlice({
                 })
             }
             else {
-                
+                existingItem.quantity++,
+                    
             }
         }
     }
