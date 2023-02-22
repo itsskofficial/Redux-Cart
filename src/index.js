866ae83d544e2867de82aeb9df3de1d4b0a1792c
SK <sarthakkarandikar03@gmail.com>
2023-02-22 11:12:33 +0000

    Feb 22, 2023, 4:42 PM

--- src/index.js
+++ src/index.js
@@ -6,5 +6,4 @@ import App from './App';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store=store={store
-}<App />);
+root.render(<Provider store={store}<App />);
