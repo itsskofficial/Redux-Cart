6c30f00b9a3d93440d9746512ed1f22fbddaffdd
SK <sarthakkarandikar03@gmail.com>
2023-02-22 11:11:55 +0000

    Feb 22, 2023, 4:41 PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { Provider } from 'react';
 
 function App() {
   return (
