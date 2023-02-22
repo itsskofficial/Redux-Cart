464e2cd04a472e8df38006ff30fb150bd07755ac
SK <sarthakkarandikar03@gmail.com>
2023-02-22 11:14:34 +0000

    Feb 22, 2023, 4:44 PM

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -2,7 +2,8 @@ import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   return (
-    <button className={classes.button} onClick={toggleCartHandler}>
+    <button className={classes.button} onClick={toggleCartHandler
+    }>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
