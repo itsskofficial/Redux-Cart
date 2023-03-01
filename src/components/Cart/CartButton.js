aa27631167b551b2d71aa3ce49f0740cc0164978
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:37:23 +0000

    Mar 1, 2023, 6:07 AM

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -4,14 +4,14 @@ import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
   const dispatch=useDispatch()
-  const totalQuantity = useSelector(state=>)
+  const totalQuantity = useSelector(state=>state.cart.totalQuantity)
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle())
   }
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{tot}</span>
     </button>
   );
 };
