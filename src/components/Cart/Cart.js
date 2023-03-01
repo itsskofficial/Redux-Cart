519f5ff45783e4255af79221692e884f162397cc
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:22:50 +0000

    Mar 1, 2023, 5:52 AM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,34 +2,13 @@ import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
-const DUMMY_ITEMS = [
-  {
-  id: 'p1',
-  quantity:1,
-  title:'Harry Potter',
-  price: 3
-  },
-  {
-  id: 'p2',
-  quantity:1,
-  title:'Percy Jacksonr',
-  price: 5
-  },
-  {
-  id: 'p3',
-  quantity:1,
-  title:'DaVinci Code',
-  price: 7
-  },
-]
-
 const Cart = (props) => {
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
         <CartItem
-          item=
+          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
         />
       </ul>
     </Card>
