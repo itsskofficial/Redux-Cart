afc2ede04b910ad16cb29f0f9dc5628af32a77f7
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:16:59 +0000

    Mar 1, 2023, 5:46 AM

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import uiSlice from "./ui-slice"
 
 const store = configureStore({
-    reducer: { 'ui': uiSlice.reducer, 'cart' }
+    reducer: { 'ui': uiSlice.reducer, 'cart' : cartSlice.reducer }
 })
 
 export default store
