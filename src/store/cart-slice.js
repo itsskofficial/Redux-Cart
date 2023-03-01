d048447a3ad7265600b20746c2df51b188aa93ba
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:38:43 +0000

    Mar 1, 2023, 6:08 AM

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,7 +10,7 @@ const cartSlice = createSlice({
         addItem(state,action) {
             const newItem = action.payload
             const existingItem = state.items.find(item => item.id === newItem.id)
-            
+            state.totalQuantity++
             if (!existingItem) {
                 state.items.push({
                     id: newItem.id,
