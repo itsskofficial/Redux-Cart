53a92b426bd02168790c725126a2a89c176dcbd0
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:22:57 +0000

    Mar 1, 2023, 5:52 AM

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,6 +1,27 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_ITEMS = [
+  {
+  id: 'p1',
+  quantity:1,
+  title:'Harry Potter',
+  price: 3
+  },
+  {
+  id: 'p2',
+  quantity:1,
+  title:'Percy Jacksonr',
+  price: 5
+  },
+  {
+  id: 'p3',
+  quantity:1,
+  title:'DaVinci Code',
+  price: 7
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
