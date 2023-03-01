c2b403aee4cd6f7085d008a64649cca1f0640c37
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:30:11 +0000

    Mar 1, 2023, 6:00 AM

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -29,6 +29,7 @@ const Products = (props) => {
       <ul>
         {DUMMY_PRODUCTS.map((product) => (
           <ProductItem
+          id
           title={product.title}
           price={product.price}
           description={product.description}
