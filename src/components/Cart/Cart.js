5b6ed69e1605db6de0c3a229129eda59843d70a8
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:41:33 +0000

    Mar 1, 2023, 6:11 AM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import CartItem from './CartItem';
 import { useSelector } from 'react-redux';
 
 const Cart = (props) => {
+  const items= useSelector(state=>state.cart.items)
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
