ecff20f0a44a1b234aa055d0dadf2ee314617bbc
SK <sarthakkarandikar03@gmail.com>
2023-03-01 00:17:41 +0000

    Mar 1, 2023, 5:47 AM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,8 @@ import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
+const DUMMY_
+
 const Cart = (props) => {
   return (
     <Card className={classes.cart}>
