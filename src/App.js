0a0c39fae7f317bc99da4b45fbf047e808c1b6ac
SK <sarthakkarandikar03@gmail.com>
2023-03-08 04:14:17 +0000

    Mar 8, 2023, 9:44 AM

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from 'react-redux'
+import { useEffect } from 'react';
 
   function App() {
   const showCart=useSelector(state=>state.ui.showCart)
