e4a8e33993f01a4e71cfbcb42f8499489582d07e
SK <sarthakkarandikar03@gmail.com>
2023-03-09 05:17:21 +0000

    Mar 9, 2023, 10:47 AM

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,7 +5,8 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
-        totalQuantity:0
+        totalQuantity: 0,
+        changed:false
     },
     reducers: {
         replaceCart(state, action) {
